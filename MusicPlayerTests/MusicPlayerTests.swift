89b5268dba97f7fa9daa8c54690436883bde067c
siddarthsingotam <siddarthsingotam1213@gmail.com>
2024-11-20 14:00:07 +0200

    Initial Commit

---
+++ MusicPlayerTests/MusicPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerTests.swift
+//  MusicPlayerTests
+//
+//  Created by Siddarth Singotam on 20.11.2024.
+//
+
+import Testing
+@testable import MusicPlayer
+
+struct MusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
